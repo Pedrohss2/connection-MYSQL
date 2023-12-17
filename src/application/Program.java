a77b2d0dbc6daba81efa017ba43f97db25f8736d
Pedro Henrique <pedro2266809ph@gmail.com>
2023-12-17 15:34:52 -0300

    Recuperar dados

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,11 +3,32 @@ package application;
 import db.DB;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Program {
     public static void main(String[] args) {
 
-        Connection connection = DB.getConnection();
-        DB.closeConnection();
+        Connection connection =  null;
+
+        Statement statement = null;
+
+        ResultSet resultSet = null;
+
+        try {
+            connection = DB.getConnection();
+            statement = connection.createStatement();
+
+            resultSet = statement.executeQuery("select  * from  seller");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt("Id") + " , " + resultSet.getString("Name"));
+            }
+        }
+        catch (SQLException error) {
+            error.printStackTrace();
+        }
+
     }
 }
